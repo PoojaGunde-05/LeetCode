a6bee6241537b554a33411cae6aad3fd3a5a8797
POOJA GUNDE <poojabgunde@gmail.com>
2026-07-23 10:32:19 +0530

    Time: 137 ms (5.04%) | Memory: 47.8 MB (57.08%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int n = arr.length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[count++] = arr[i];
+            }
+        }
+        for (int i = count; i < n; i++) {
+            arr[i] = 0;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        
+    }
+}
